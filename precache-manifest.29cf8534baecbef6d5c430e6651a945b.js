c9ec1fd3762423a47bf689933975baafb35030ff
Noah Potter <noah.potter@outlook.com>
2019-09-04 19:17:48 -0600

    Updates

---
+++ precache-manifest.29cf8534baecbef6d5c430e6651a945b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07042982c00adc124900bfdf869a5cd1",
+    "url": "/connect-four/index.html"
+  },
+  {
+    "revision": "b81d20523f2f2a7c3fa7",
+    "url": "/connect-four/static/js/2.40b54f1b.chunk.js"
+  },
+  {
+    "revision": "249c2c7b91edfdec5bd6",
+    "url": "/connect-four/static/js/main.229852b4.chunk.js"
+  },
+  {
+    "revision": "556e3fb2475eab09d753",
+    "url": "/connect-four/static/js/runtime~main.306b70a0.js"
+  }
+]);
